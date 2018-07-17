c161cb59e2411936debf035b4e21cd710a090fb4
yifu <yifu@thoughtworks.com>
2018-07-17 23:03:51 +0800

    [fuyi] generate the word list

--- main/main.js
+++ main/main.js
@@ -1,4 +1,31 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+
+const {loadAllItems, loadPromotions} = require('./datbase.js');
+
+const allItems = loadAllItems();
+const promotions = loadPromotions();
+let wordList = [];
+let expectText = '';
+let promotionPrice = 0;
+
+module.exports = function main(barCode) {
+    generateWordList(barCode);
+    console.log(expectText);
 };
+
+function generateWordList(barCode) {
+    let temptItems = allItems;
+    barCode.forEach(value => {
+        temptItems.map(item => {
+            let barCode = item.barcode;
+            if (value.indexOf(barCode) !== -1) {
+                if (item.count === undefined) item.count = 0;
+                value.indexOf("-") !== -1 ?
+                    item.count = parseInt(value.substr(value.length - 1)) :
+                    item.count++;
+                item.totalPrince = item.count * item.price;
+            }
+        })
+    });
+    wordList = temptItems.filter(item => item.count !== undefined);
+}
+
